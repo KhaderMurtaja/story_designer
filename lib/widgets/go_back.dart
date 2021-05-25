6b74f9f9393a3245727bc4a56b73e6e510502b8c
KhaderMurtaja <khadermurtaja1@gmail.com>
2021-05-25 14:34:03 +0300

    Refactor code

---
+++ lib/widgets/go_back.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class GoBack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Icon(
+        Icons.arrow_back,
+        size: 28,
+        color: Colors.white,
+      ),
+      onTap: () {
+        if (Navigator.canPop(context)) {
+          Navigator.pop(context);
+        } else {
+          SystemNavigator.pop();
+        }
+      },
+    );
+  }
+}
